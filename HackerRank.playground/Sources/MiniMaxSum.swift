1e781651caedacbe7bbdc86b7a8daa6a64512480
lpolo <lpolo@nelogica.com.br>
2021-02-17 10:29:51 -0300

    Mini Max Sum

---
+++ HackerRank.playground/Sources/MiniMaxSum.swift
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+import Foundation
+
+func miniMaxSum(arr: [Int]) -> Void {
+    var minIndex = 0
+    var maxIndex = 0
+    var sum = 0
+
+    for i in 0...arr.count - 1 {
+        if arr[i] < arr[minIndex] {
+            minIndex = i
+        } else if arr[i] > arr[maxIndex] {
+            maxIndex = i
+        }
+        sum += arr[i]
+    }
+
+    print(sum - arr[maxIndex], sum - arr[minIndex])
+
+}
