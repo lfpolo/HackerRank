3bcbf164b3b7c926f7689e92f98c53036d81c322
lpolo <lpolo@nelogica.com.br>
2021-02-17 09:44:56 -0300

    PlusMinus

---
+++ HackerRank.playground/Sources/PlusMinus.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+///problem: https://www.hackerrank.com/challenges/plus-minus/problem
+
+func plusMinus(arr: [Int]) -> Void {
+    var positives: Float = 0
+    var negatives: Float = 0
+    var zero: Float = 0
+
+    for n in arr {
+        if n > 0 {
+            positives += 1
+        } else if n < 0 {
+            negatives += 1
+        } else {
+            zero += 1
+        }
+    }
+
+    let total : Float = positives + negatives + zero
+
+    print(String(format: "%.6f", positives/total))
+    print(String(format: "%.6f", negatives/total))
+    print(String(format: "%.6f", zero/total))
+}
